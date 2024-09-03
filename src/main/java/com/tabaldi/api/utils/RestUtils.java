e34d77c4d74e53b85843720bad55ffbec422294a
maizn_hashim <maz.hash03@gmail.com>
2024-09-03 15:18:51 +0300

    added logging for all errors

--- src/main/java/com/tabaldi/api/utils/RestUtils.java
+++ src/main/java/com/tabaldi/api/utils/RestUtils.java
@@ -1,7 +1,10 @@
 package com.tabaldi.api.utils;
 
 import com.tabaldi.api.exception.TabaldiGenericException;
+import com.tabaldi.api.serviceImpl.PaymentServiceImpl;
 import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
@@ -12,12 +15,14 @@ import org.springframework.web.client.RestTemplate;
 @NoArgsConstructor
 public class RestUtils {
     private static final RestTemplate restTemplate = new RestTemplate();
+    final static Logger logger = LoggerFactory.getLogger(RestUtils.class);
 
     public static <T> T postRequest(String url, @Nullable Object request, Class<T> responseType, int exceptionCode, String exceptionMessage) throws HttpClientErrorException, TabaldiGenericException {
         ResponseEntity<T> entity = restTemplate.postForEntity(url, request, responseType, new Object[0]);
         if (entity.getStatusCode().equals(HttpStatus.OK) && entity.getBody() != null) {
             return entity.getBody();
         } else {
+            logger.error(entity.getBody().toString());
             throw new TabaldiGenericException(exceptionCode, exceptionMessage);
         }
     }
