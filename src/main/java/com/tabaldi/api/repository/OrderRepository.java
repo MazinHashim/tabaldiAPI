b29bf0028509fd2a88d91126b58357ef95b831ee
maizn_hashim <maz.hash03@gmail.com>
2024-11-09 02:10:06 +0300

    fetched only the orders that has created invoices to ensure application stability

--- src/main/java/com/tabaldi/api/repository/OrderRepository.java
+++ src/main/java/com/tabaldi/api/repository/OrderRepository.java
@@ -17,15 +17,20 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query(value = "select count(*) from orders where DATE(order_date) = CURRENT_DATE", nativeQuery = true)
     long countByOrderDate();
     @Query("select o from Order o where o.customer.customerId = ?1 and o.vendor.vendorId = ?2 and " +
-            "o.status not in ('DELIVERED', 'CANCELED') ORDER BY o.orderDate DESC")
+            "o.status not in ('DELIVERED', 'CANCELED') and o.orderId in (select inv.order.orderId from Invoice inv) " +
+            "ORDER BY o.orderDate DESC")
     List<Order> getLastActiveOrderPerVendor(long customerId, long vendorId);
 
-    @Query("select o from Order o where o.status not in ?1")
+    @Query("select o from Order o where o.status not in ?1 and " +
+            "o.orderId in (select inv.order.orderId from Invoice inv)")
     List<Order> findByPendingOrders(Collection<OrderStatus> statuses);
-    @Query("select o from Order o where o.status not in ?1 and o.customer.customerId=?2")
+    @Query("select o from Order o where o.status not in ?1 and o.customer.customerId=?2 " +
+            "and o.orderId in (select inv.order.orderId from Invoice inv)")
     List<Order> findPendingOrdersByCustomer(Collection<OrderStatus> statuses, long customerId);
-
+    @Query("select o from Order o where o.vendor = ?1 " +
+            "and o.orderId in (select inv.order.orderId from Invoice inv)")
     List<Order> findByVendor(Vendor vendor);
-
+    @Query("select o from Order o where o.customer = ?1 " +
+            "and o.orderId in (select inv.order.orderId from Invoice inv)")
     List<Order> findByCustomer(Customer customer);
 }
